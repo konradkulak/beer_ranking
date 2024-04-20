4a33472c6df536527abda6c13ee3af8057940740
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-04-20 09:46:41 +0200

    Add home model

---
+++ lib/domain/models/home_model.dart
@@ -0,0 +1,11 @@
+class HomeModel {
+  HomeModel({
+    required this.name,
+    required this.brewery,
+    required this.rating,
+  });
+
+  final String name;
+  final String brewery;
+  final double rating;
+}
