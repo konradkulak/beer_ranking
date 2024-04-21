2a6db1f7ec3fc66c6a5599d7358fdd9a63373596
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-04-21 10:24:44 +0200

    Create add_page

---
+++ lib/features/add/pages/add_page.dart
@@ -0,0 +1,28 @@
+import 'package:beer_ranking/features/auth/pages/user_profile_page.dart';
+import 'package:flutter/material.dart';
+
+class AddPage extends StatelessWidget {
+  const AddPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Beer Ranking'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const UserProfile(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.person),
+          ),
+        ],
+      ),
+      body: const Text('test'),
+    );
+  }
+}
