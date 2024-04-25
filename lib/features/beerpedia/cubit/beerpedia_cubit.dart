63751c3a9f76c8b7d5398ddc94e550fe5aa31c72
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-04-25 21:55:32 +0200

    Implement bloc logic for Beerpedia incl. handling status, error and loading

---
+++ lib/features/beerpedia/cubit/beerpedia_cubit.dart
@@ -0,0 +1,35 @@
+import 'package:beer_ranking/app/core/enums.dart';
+import 'package:beer_ranking/domain/models/beerpedia_model.dart';
+import 'package:beer_ranking/domain/repositories/beerpedia_repository.dart';
+
+import 'package:bloc/bloc.dart';
+
+part 'beerpedia_state.dart';
+
+class BeerpediaCubit extends Cubit<BeerpediaState> {
+  BeerpediaCubit(this._beerpediaRepository) : super(BeerpediaState());
+
+  final BeerpediaRepository _beerpediaRepository;
+
+  Future<void> getBeerpediaModel({
+    required String beer,
+  }) async {
+    emit(BeerpediaState(beerpediaStatus: BeerpediaStatus.loading));
+    try {
+      final beerpediaModel =
+          await _beerpediaRepository.getBeerpediaModel(beer: beer);
+      emit(
+        BeerpediaState(
+            beerpediaModel: beerpediaModel,
+            beerpediaStatus: BeerpediaStatus.success),
+      );
+    } catch (error) {
+      emit(
+        BeerpediaState(
+          beerpediaStatus: BeerpediaStatus.error,
+          errorMessage: error.toString(),
+        ),
+      );
+    }
+  }
+}
