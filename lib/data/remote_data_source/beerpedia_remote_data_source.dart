f4940ba6f5fa94ed5773bac613c3060faa385de0
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-04-27 13:20:23 +0200

    Change API endpoint. Modify getBeerpediaData method to use new API endpoint. Add data filtering to filter response by the beer title.

--- lib/data/remote_data_source/beerpedia_remote_data_source.dart
+++ lib/data/remote_data_source/beerpedia_remote_data_source.dart
@@ -1,16 +1,52 @@
 import 'package:dio/dio.dart';
 
 class BeerpediaRemoteDataSource {
-  Future<Map<String, dynamic>?> getBeerpediaData({
-    required String beer,
-  }) async {
+  late Dio _dio;
+
+  BeerpediaRemoteDataSource() {
+    _dio = Dio(
+      BaseOptions(baseUrl: 'https://beers-list.p.rapidapi.com', headers: {
+        'X-RapidAPI-Key': 'ca6d4587c0msh615181cdbec7b83p1d0716jsn74c6e7a341c6',
+        'X-RapidAPI-Host': 'beers-list.p.rapidapi.com',
+      }),
+    );
+  }
+
+  String encodeQuery(String query) {
+    return Uri.encodeComponent(query);
+  }
+
+  Future<List<dynamic>?> getBeerpediaData(String title) async {
+    String encodedTitle = encodeQuery(title);
     try {
-      final response = await Dio().get<Map<String, dynamic>>(
-          'https://beer-lover.p.rapidapi.com/search/beer/$beer?rapidapi-key=ca6d4587c0msh615181cdbec7b83p1d0716jsn74c6e7a341c6');
-      return response.data;
+      final response =
+          await _dio.get('/beers', queryParameters: {'title': encodedTitle});
+      if (response.statusCode == 200 && response.data is List) {
+        var filteredData = (response.data as List).where((element) {
+          return element['title']
+              .toString()
+              .toLowerCase()
+              .contains(title.toLowerCase());
+        }).toList();
+        if (filteredData.isNotEmpty) {
+          return filteredData;
+        } else {
+          print('No matching data found');
+          return null;
+        }
+      } else {
+        print('Failed to load data: ${response.statusCode}');
+        return null;
+      }
     } on DioException catch (error) {
-      throw Exception(
-          error.response?.data['error']['message'] ?? 'Unknown error');
+      print('DioException caught: ${error.message}');
+      if (error.response != null) {
+        print('Error data: ${error.response?.data}');
+      }
+      return null;
+    } catch (error) {
+      print('An unexpected error occurred: $error');
+      return null;
     }
   }
 }
