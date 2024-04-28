ff2b7d7e3a0d536efdd0f9597d8e2c1944d1d6bd
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-04-28 19:03:30 +0200

    Implement firebase auth methods in auth data source

---
+++ lib/data/remote_data_source/auth_remote_data_source.dart
@@ -0,0 +1,28 @@
+import 'package:beer_ranking/domain/models/auth_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthRemoteDataSource {
+  AuthRemoteDataSource();
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<AuthModel> signInWithEmailAndPassword(
+      String email, String password) async {
+    var result = await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+    return AuthModel(
+      id: result.user!.uid,
+      email: result.user!.uid,
+    );
+  }
+
+  Future<AuthModel> registerWithEmailAndPassword(
+      String email, String password) async {
+    var result = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+    return AuthModel(
+      id: result.user!.uid,
+      email: result.user!.email!,
+    );
+  }
+}
