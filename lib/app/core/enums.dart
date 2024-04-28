1439e87d15d98a2e5f586c12452910520710dea1
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-04-28 18:51:44 +0200

    Create enum for auth status.

--- lib/app/core/enums.dart
+++ lib/app/core/enums.dart
@@ -9,3 +9,5 @@ enum InfoStatus { loading, success, error }
 enum ImageStatus { valid, missing, malformed, dataURI }
 
 enum BeerpediaStatus { initial, loading, success, error }
+
+enum AuthStatus { initial, loading, success, error }
