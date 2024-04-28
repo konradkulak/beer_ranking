d6fd383828b883b2affabef33df7ee2d492bab51
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-04-28 19:00:08 +0200

    Change firestore security rules and modify Beer data source for user-specific data (collection) access.

--- lib/data/remote_data_source/beer_remote_data_source.dart
+++ lib/data/remote_data_source/beer_remote_data_source.dart
@@ -1,9 +1,16 @@
 import 'package:beer_ranking/domain/models/beer_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class BeerRemoteDataSource {
   Stream<List<Map<String, dynamic>>> getBeerStream() {
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
     return FirebaseFirestore.instance
+        .collection('users')
+        .doc(userID)
         .collection('items')
         .orderBy('rating', descending: true)
         .snapshots()
@@ -20,8 +27,17 @@ class BeerRemoteDataSource {
   }
 
   Future<void> deleteItem(String id) async {
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
     try {
-      await FirebaseFirestore.instance.collection('items').doc(id).delete();
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userID)
+          .collection('items')
+          .doc(id)
+          .delete();
     } catch (error) {
       throw Exception(
         'Failed to delete item: ${error.toString()}',
@@ -30,8 +46,16 @@ class BeerRemoteDataSource {
   }
 
   Future<void> addItem(BeerModel beer) async {
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
     try {
-      await FirebaseFirestore.instance.collection('items').add(beer.toMap());
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userID)
+          .collection('items')
+          .add(beer.toMap());
     } catch (error) {
       throw Exception(
         'Failed to add item: ${error.toString()}',
@@ -40,9 +64,17 @@ class BeerRemoteDataSource {
   }
 
   Future<BeerModel> getBeerID(String id) async {
+    final userID = FirebaseAuth.instance.currentUser?.uid;
+    if (userID == null) {
+      throw Exception('User is not logged in');
+    }
     try {
-      DocumentSnapshot doc =
-          await FirebaseFirestore.instance.collection('items').doc(id).get();
+      DocumentSnapshot doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userID)
+          .collection('items')
+          .doc(id)
+          .get();
       if (doc.exists) {
         Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
         data['id'] = doc.id;
