43e29b9144de06960957eec97acb5770c10b6659
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-05-01 08:59:54 +0200

    Refine API data formating in BeerpediaModel - single spaces between words in "despription" and capital letter of country names in "country". To improve UI clarity.

--- lib/domain/models/beerpedia_model.dart
+++ lib/domain/models/beerpedia_model.dart
@@ -14,6 +14,15 @@ class BeerpediaModel {
   BeerpediaModel.fromJson(Map<String, dynamic> json)
       : title = json['title'],
         alcohol = json['alchool'],
-        description = json['description'],
-        country = json['country'];
+        description = _formatDescription(json['description']),
+        country = _capitalizeFirstLetter(json['country']);
+}
+
+String _formatDescription(String description) {
+  return description.replaceAll(RegExp(r'\s+'), ' ').trim();
+}
+
+String _capitalizeFirstLetter(String country) {
+  if (country.isEmpty) return country;
+  return country[0].toUpperCase() + country.substring(1).toLowerCase();
 }
