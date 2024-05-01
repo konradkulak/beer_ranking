7473e35d7b69c4dc92a2d64b3b9c3d02842b8471
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-05-01 20:29:31 +0200

    Implement input validation for email (format) and password (min 6 characters) in AuthCubit.

--- lib/features/auth/cubit/auth_cubit.dart
+++ lib/features/auth/cubit/auth_cubit.dart
@@ -2,6 +2,7 @@ import 'package:beer_ranking/app/core/enums.dart';
 import 'package:beer_ranking/domain/models/auth_model.dart';
 import 'package:beer_ranking/domain/repositories/auth_repository.dart';
 import 'package:bloc/bloc.dart';
+import 'package:email_validator/email_validator.dart';
 
 part 'auth_state.dart';
 
@@ -11,6 +12,21 @@ class AuthCubit extends Cubit<AuthState> {
   final AuthRepository _authRepository;
 
   Future<void> signIn(String email, String password) async {
+    if (!EmailValidator.validate(email)) {
+      emit(
+        AuthState(
+            authStatus: AuthStatus.error, errorMessage: 'Invalid email format'),
+      );
+      return;
+    }
+    if (password.length < 6) {
+      emit(
+        AuthState(
+            authStatus: AuthStatus.error,
+            errorMessage: 'Password must be at least 6 characters'),
+      );
+      return;
+    }
     emit(
       AuthState(
         authStatus: AuthStatus.loading,
@@ -36,6 +52,21 @@ class AuthCubit extends Cubit<AuthState> {
   }
 
   Future<void> register(String email, String password) async {
+    if (!EmailValidator.validate(email)) {
+      emit(
+        AuthState(
+            authStatus: AuthStatus.error, errorMessage: 'Invalid email format'),
+      );
+      return;
+    }
+    if (password.length < 6) {
+      emit(
+        AuthState(
+            authStatus: AuthStatus.error,
+            errorMessage: 'Password must be at least 6 characters'),
+      );
+      return;
+    }
     emit(
       AuthState(
         authStatus: AuthStatus.loading,
