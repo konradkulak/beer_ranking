ac88ec7d93080aa3ae441ad3ed7680ec35fa0dd9
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-05-26 08:03:39 +0200

    Update MainActivity to register Flutter plugins

--- android/app/src/main/kotlin/com/example/beer_ranking/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/beer_ranking/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.konradkulak.beer_ranking
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
