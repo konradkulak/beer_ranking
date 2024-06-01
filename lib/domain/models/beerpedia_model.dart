bc741dc253a42424f669118e26314e278cceafe2
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-06-01 11:41:22 +0200

    Refactor BeerpediaModel to use freezed

--- lib/domain/models/beerpedia_model.dart
+++ lib/domain/models/beerpedia_model.dart
@@ -1,28 +1,46 @@
-class BeerpediaModel {
-  BeerpediaModel({
-    required this.title,
-    required this.alcohol,
-    required this.description,
-    required this.country,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  final String title;
-  final String alcohol;
-  final String description;
-  final String country;
+part 'beerpedia_model.freezed.dart';
+part 'beerpedia_model.g.dart';
 
-  BeerpediaModel.fromJson(Map<String, dynamic> json)
-      : title = json['title'] ?? '',
-        alcohol = json['alcohol'] ?? '',
-        description = _formatDescription(json['description'] ?? ''),
-        country = _capitalizeFirstLetter(json['country'] ?? '');
+@freezed
+class BeerpediaModel with _$BeerpediaModel {
+  factory BeerpediaModel(
+    String title,
+    String alcohol,
+    String description,
+    String country,
+  ) = _BeerpediaModel;
+
+  factory BeerpediaModel.fromJson(Map<String, dynamic> json) =>
+      _$BeerpediaModelFromJson(json);
 }
 
-String _formatDescription(String description) {
-  return description.replaceAll(RegExp(r'\s+'), ' ').trim();
-}
+// class BeerpediaModel {
+//   BeerpediaModel({
+//     required this.title,
+//     required this.alcohol,
+//     required this.description,
+//     required this.country,
+//   });
 
-String _capitalizeFirstLetter(String country) {
-  if (country.isEmpty) return country;
-  return country[0].toUpperCase() + country.substring(1);
-}
+//   final String title;
+//   final String alcohol;
+//   final String description;
+//   final String country;
+
+//   BeerpediaModel.fromJson(Map<String, dynamic> json)
+//       : title = json['title'] ?? '',
+//         alcohol = json['alcohol'] ?? '',
+//         description = _formatDescription(json['description'] ?? ''),
+//         country = _capitalizeFirstLetter(json['country'] ?? '');
+// }
+
+// String _formatDescription(String description) {
+//   return description.replaceAll(RegExp(r'\s+'), ' ').trim();
+// }
+
+// String _capitalizeFirstLetter(String country) {
+//   if (country.isEmpty) return country;
+//   return country[0].toUpperCase() + country.substring(1);
+// }
