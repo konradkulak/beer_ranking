2da1bedfa602a7e32396ebda27d7fad1c637f415
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-06-03 16:13:53 +0200

    Refactor AuthModel to use freezed

--- lib/domain/models/auth_model.dart
+++ lib/domain/models/auth_model.dart
@@ -1,9 +1,15 @@
-class AuthModel {
-  AuthModel({
-    required this.id,
-    required this.email,
-  });
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  final String id;
-  final String email;
+part 'auth_model.freezed.dart';
+part 'auth_model.g.dart';
+
+@freezed
+class AuthModel with _$AuthModel {
+  factory AuthModel(
+    String id,
+    String email,
+  ) = _AuthModel;
+
+  factory AuthModel.fromJson(Map<String, dynamic> json) =>
+      _$AuthModelFromJson(json);
 }
