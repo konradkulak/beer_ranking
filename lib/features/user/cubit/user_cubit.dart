68ecaa69c43fd756be2cc89d290702ad57522292
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-06-11 09:37:25 +0200

    Refactor UserCubit and UserState to use freezed

--- lib/features/user/cubit/user_cubit.dart
+++ lib/features/user/cubit/user_cubit.dart
@@ -1,24 +1,32 @@
 import 'package:beer_ranking/app/core/enums.dart';
 import 'package:beer_ranking/domain/repositories/auth_repository.dart';
 import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user_state.dart';
+part 'user_cubit.g.dart';
+part 'user_cubit.freezed.dart';
 
 class UserCubit extends Cubit<UserState> {
-  UserCubit(this._authRepository) : super(UserState());
+  UserCubit(this._authRepository)
+      : super(
+          const UserState(
+            userStatus: UserStatus.initial,
+          ),
+        );
 
   final AuthRepository _authRepository;
 
   Future<void> signOut() async {
     emit(
-      UserState(
+      const UserState(
         userStatus: UserStatus.loading,
       ),
     );
     try {
       await _authRepository.signOut();
       emit(
-        UserState(
+        const UserState(
           userStatus: UserStatus.success,
         ),
       );
@@ -31,16 +39,17 @@ class UserCubit extends Cubit<UserState> {
       );
     }
   }
-    Future<void> deleteAccount() async {
+
+  Future<void> deleteAccount() async {
     emit(
-      UserState(
+      const UserState(
         userStatus: UserStatus.loading,
       ),
     );
     try {
       await _authRepository.deleteAccount();
       emit(
-        UserState(
+        const UserState(
           userStatus: UserStatus.success,
         ),
       );
