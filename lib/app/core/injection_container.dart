cd336d0e657191f64715c3eabac952da38a50931
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-06-13 12:32:16 +0200

    Create injection container and register AddCubit, BeerRepository and BeerRemoteDataSource as a factory

---
+++ lib/app/core/injection_container.dart
@@ -0,0 +1,17 @@
+import 'package:beer_ranking/data/remote_data_source/beer_remote_data_source.dart';
+import 'package:beer_ranking/domain/repositories/beer_repository.dart';
+import 'package:beer_ranking/features/add/cubit/add_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void configureDependencies() {
+  //Data Sources
+  getIt.registerFactory(() => BeerRemoteDataSource());
+
+  //Repositories
+  getIt.registerFactory(() => BeerRepository(getIt<BeerRemoteDataSource>()));
+
+  //Bloc
+  getIt.registerFactory(() => AddCubit(getIt<BeerRepository>()));
+}
