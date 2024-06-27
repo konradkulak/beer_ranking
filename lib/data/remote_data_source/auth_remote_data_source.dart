e39832bf2cc55639bec9b2fbd7bfefd2c170b15c
Konrad Kulak <konrad.kulak.95@gmail.com>
2024-06-27 18:52:32 +0200

    Add injectable annotation to AuthRemoteDataSource

--- lib/data/remote_data_source/auth_remote_data_source.dart
+++ lib/data/remote_data_source/auth_remote_data_source.dart
@@ -1,6 +1,8 @@
 import 'package:beer_ranking/domain/models/auth_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:injectable/injectable.dart';
 
+@injectable
 class AuthRemoteDataSource {
   AuthRemoteDataSource();
 
